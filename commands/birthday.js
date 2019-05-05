ccd9562d59dae879bdfb69248b23813cdc8d2da0
Adi Avraham <30277165+HalfALifeLeft@users.noreply.github.com>
2019-05-05 10:42:10 -0700

    fixed birthday shout outs

--- commands/birthday.js
+++ commands/birthday.js
@@ -6,36 +6,38 @@ module.exports.run = async (client, message, args) => {
         birthdayTS: ``,
         birthdayMonth: ``,
         birthdayDay: ``,
-        currentYear: ``,
+        currentYear: `2018`,
         userID: ``
     });
 
-    var birthday = args[0];
     let months = [`January`, `February`, `March`, `April`, `May`, `June`, `July`, `August`, `September`, `October`, `November`, `December`]; //Just an array of the months
-    let dateFormat1 = /\b(\d{2}\/\d{2}\/\d{4})\b/g //regex to test for mm/dd/yyyy format
+    let dateFormat1 = /\b(\d{2}\/\d{2}\/\d{4})\b/g; //regex to test for mm/dd/yyyy format
     let separated = [];
 
-    if (dateFormat1.test(args[0]) === false) {
-        return message.reply(`Please give me your birthday in the mm/dd/yyyy format!`);
+    if (args[0]) {
+
+        if (dateFormat1.test(args[0]) === false) {
+            return message.reply(`Please give me your birthday in the mm/dd/yyyy format!`);
+        }
+
+        separated.push(parseInt(args[0].slice(0, 2)) - 1);
+        separated.push(parseInt(args[0].slice(3, 5)));
+        separated.push(parseInt(args[0].slice(6, 10)));
+
+        //Creates array of birthday - 0 = mm -  1 = dd - 2 = yyyy
+        var date = Date.UTC(separated[2], separated[0], separated[1]);
+
+        message.channel.send(`Set ${months[parseInt(separated[0])]} ${separated[1]}, ${separated[2]} as your birthday.`);
+
+        client.birthday.set(key, `${date}`, `birthdayTS`);
+        client.birthday.set(key, `${parseInt(separated[0])}`, `birthdayMonth`);
+        client.birthday.set(key, `${separated[1]}`, `birthdayDay`);
+        //client.birthday.set(key, `${now.getFullYear()}`, `currentYear`);
+        client.birthday.set(key, `${message.author.id}`, `userID`);
+
+    } else {
+        message.channel.send(`Your birthday is set to ${months[parseInt(client.birthday.get(key, `birthdayMonth`))]} ${parseInt(client.birthday.get(key, `birthdayDay`))}.`);
     }
-
-    separated.push(parseInt(args[0].slice(0,2)) - 1);
-    separated.push(parseInt(args[0].slice(3,5)));
-    separated.push(parseInt(args[0].slice(6,10)));
-
-    //Creates array of birthday - 0 = mm -  1 = dd - 2 = yyyy
-    var date = Date.UTC(separated[2], separated[0], separated[1]);
-    var now = new Date(Date.now());
-
-    message.channel.send(`Set ${months[parseInt(separated[0])]} ${separated[1]}, ${separated[2]} as your birthday.`);
-
-    client.birthday.set(key, `${date}`, `birthdayTS`);
-    client.birthday.set(key, `${parseInt(separated[0])}`, `birthdayMonth`);
-    client.birthday.set(key, `${separated[1]}`, `birthdayDay`);
-    client.birthday.set(key, `${now.getFullYear()}`, `currentYear`);
-    client.birthday.set(key, `${message.author.id}`, `userID`);
-
-    console.log(client.birthday.get(key));
 };
 module.exports.help = {
     name: `birthday`
--- events/ready.js
+++ events/ready.js
@@ -2,34 +2,63 @@ module.exports = (client) => {
     // eslint-disable-next-line no-console
     console.log(`Ready to serve in ${client.channels.size} channels on ${client.guilds.size} servers, for a total of ${client.users.size} users.`);
 
-    client.users.find(u => u.id === `444384280152637441`).send(`Bot started up/restarted!`);
-    setInterval(function () {
-        //checks all guilds
-        client.guilds.forEach((guild) => {
-            //checks all members in the said guild
-            guild.members.forEach((member) => {
-                var date = new Date(Date.now());
-                const key = member.id;
-    
-                //check if the member even has a birthday logged
-                if (!client.birthday.has(member.id)) {
-                    return;
-                    //No need to waste CPU time!
-                }
-    
-                //check if the birthday month matches today's month
-                if (date.getMonth() == client.birthday.get(key, `birthdayMonth`)) {
-                    //check if the birthday day matches today's day
-                    if (date.getDate() == client.birthday.get(key, `birthdayDay`)) {
-                        //This is our announce channel! Yeah!
-                        let announceChannel = guild.channels.find(ch => ch.id === client.roles.get(guild.id, `announceChannel`));
-    
-                        //This is sending the announcement! Happy birthday!
-                        announceChannel.send(`It's <@!${key}>'s birthday! Wish them a happy birthday!`);
-                    }
-                }
-            });
-        });
-    }, 86400000); //run every 24 hours (86400000 ms)
+    //client.users.find(u => u.id === `444384280152637441`).send(`Bot started up/restarted!`);
 
+    setInterval(function () {
+
+        var now = new Date(Date.now());
+
+        //console.log(`setInt ran!`);
+
+        //Run at 3 PM EST, 19 UTC.
+        if (now.getUTCHours() === 19) {
+            //console.log(`It's 12!`);
+
+            //checks all guilds
+            client.guilds.forEach((guild) => {
+                //checks all members in the said guild
+
+                if (!client.roles.has(guild.id)) {
+                    //console.log(`Guild doesn't have any configurations`);
+                    return;
+                } else if (client.roles.get(guild.id, `announceChannel`) === ``) {
+                    //console.log(`Guild doesn't have a birthday channel REEE!`);
+                    return;
+                }
+                //Run a check to make sure guild has birthday channel, if not return
+
+                guild.members.forEach((member) => {
+                    var date = new Date(Date.now());
+                    const key = member.id;
+
+                    //check if the member even has a birthday logged
+                    if (!client.birthday.has(member.id)) {
+                        //console.log(`No Birthday :c`);
+                        return;
+                        //No need to waste CPU time!
+                    }
+
+                    if (now.getFullYear() != client.birthday.get(key, `currentYear`)) {
+                        //DON'T RUN IF NOT, MAKE SURE TO SET TO CURRENT YEAR AFTER BIRTHDAY ANNOUNCE
+
+                        //console.log(`WOOOOOOO BIRTHDAY!`);
+                        //check if the birthday month matches today's month
+                        if (date.getMonth() == client.birthday.get(key, `birthdayMonth`)) {
+                            //check if the birthday day matches today's day
+                            if (date.getDate() == client.birthday.get(key, `birthdayDay`)) {
+                                //This is our announce channel! Yeah!
+                                let announceChannel = guild.channels.find(ch => ch.id === client.roles.get(guild.id, `announceChannel`));
+
+                                //This is sending the announcement! Happy birthday!
+                                announceChannel.send(`It's <@!${key}>'s birthday! Wish them a happy birthday!`);
+                                client.birthday.set(key, `${date.getFullYear()}`, `currentYear`);
+                            }
+                        }
+                    } else {
+                        //console.log(`Already shouted out the birthday lmao`);
+                    }
+                });
+            });
+        }
+    }, 10000); //run every 24 hours (86400000 ms)
 };
