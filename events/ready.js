b5cb87a76d59c95f2b44c453924efd0124bb05c2
Adi Avraham <30277165+HalfALifeLeft@users.noreply.github.com>
2019-05-06 14:21:08 -0700

    ugh fixed ready. Again.

--- events/ready.js
+++ events/ready.js
@@ -1,16 +1,8 @@
 module.exports = (client) => {
     // eslint-disable-next-line no-console
     console.log(`Ready to serve in ${client.channels.size} channels on ${client.guilds.size} servers, for a total of ${client.users.size} users.`);
-
-    //client.users.find(u => u.id === `444384280152637441`).send(`Bot started up/restarted!`);
-
+    
     setInterval(function () {
-
-        //console.log(`setInt ran!`);
-
-        //Run at 3 PM EST, 19 UTC.
-        //if (now.getUTCHours() === 19) {
-        //console.log(`It's 12!`);
 
         //checks all guilds
         client.guilds.forEach((guild) => {
@@ -38,16 +30,17 @@ module.exports = (client) => {
 
                 var timestamp = client.birthday.get(key, `timestamp`);
 
-                //console.log(timestamp);
-                //console.log(date.getTime());
-
-
                 //1 Yr in MS = 31536000000
 
-                if (timestamp < date.getTime()) {
-                    //console.log(`Timestamp is less than now`);
+                function leapYear(year)
+                {
+                  return ((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0);
+                }
 
-                    //console.log(`WOOOOOOO BIRTHDAY!`);
+
+                //Check if the set timestamp is less than the current timestamp
+                if (timestamp < date.getTime()) {
+
                     //check if the birthday month matches today's month
                     if (date.getMonth() == client.birthday.get(key, `birthdayMonth`)) {
                         //check if the birthday day matches today's day
@@ -57,17 +50,23 @@ module.exports = (client) => {
 
                             //This is sending the announcement! Happy birthday!
                             announceChannel.send(`It's <@!${key}>'s birthday! Wish them a happy birthday!`);
-                            client.birthday.set(key, `${date.getFullYear()}`, `currentYear`);
+                            //client.birthday.set(key, `${date.getFullYear()}`, `currentYear`);
                         }
                     } else {
                         //console.log(`Already shouted out the birthday lmao`);
                     }
-                    client.birthday.set(key, timestamp + 31536000000, `timestamp`);
+
+                    var oneYear = 31536000000;
+
+                    if (leapYear(date.getFullYear()) == true) {
+                        oneYear = 31622400000;
+                    }
+
+                    client.birthday.set(key, timestamp + oneYear, `timestamp`);
                 } else {
                     //console.log(`Now is greater than set timestamp`);
                 }
             });
         });
-        //}
-    }, 60000); //run every 24 hours (86400000 ms)
+    }, 60000);
 };
